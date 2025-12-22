61d482f3a355e6375b40f98858fa294abc158240
simo <simo.bambou@gmail.com>
2025-12-22 22:24:54 +0100

    add testing conf

--- Filiere_Service/src/test/java/dcc/filiere_service/FiliereServiceApplicationTests.java
+++ Filiere_Service/src/test/java/dcc/filiere_service/FiliereServiceApplicationTests.java
@@ -2,8 +2,21 @@ package dcc.filiere_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(
+        properties = {
+                "spring.datasource.url=jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE",
+                "spring.datasource.driver-class-name=org.h2.Driver",
+                "spring.datasource.username=sa",
+                "spring.datasource.password=",
+                "spring.jpa.database-platform=org.hibernate.dialect.H2Dialect",
+                "spring.jpa.hibernate.ddl-auto=create-drop"
+        }
+)
+
+
+@ActiveProfiles("test")
 class FiliereServiceApplicationTests {
 
     @Test
